9dad52dcd9c896a7945d06558f0850b4a416de3f
d7o1d1s0 <d7sarmy@gmail.com>
2022-12-08 16:18:15 +0000

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App'
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
+import './assets/shards-dashboards.1.1.0.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
